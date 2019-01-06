c2b10e11b876673e4fe6311464be8d00a6b27be8
ArchangelD <sdimartino@gmail.com>
2019-01-05 18:46:57 -0800

    Adds random number generators

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,38 @@ $(document).ready(function() {
 
     //pseudocode
 
-    //
+    //Randomly generate a number between 19 and 120 and assign it to random-number-box
+
+    var minRandNumber = 19;
+    var maxRandNumber = 120;
+    var randomNumber = randomNumberRange(minRandNumber, maxRandNumber);
+    var minCrystalValue = 1;
+    var maxCrystalValue = 12;
+    var randomCrystalValue = randomCrystalValueRange(minCrystalValue, maxCrystalValue);
+
+    function randomNumberRange(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    console.log(randomNumber);
+
+    $(".random-number-box").append(randomNumber);
+
+    //Randomly generate a number between 1 and 12 and assign it to each crystal. No two crystals may have the same number assigned.
+
+    function randomCrystalValueRange(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    console.log(randomCrystalValue);
+
+    //When player clicks on a crystal, assign the score assigned to that crystal to total-score-box. Add that crystal's value to the amount in total-score-box each time it is clicked.
+
+    //compare the score in total-score-box to the number in random-number-box. if total score < random number, play continues. If total score = random number, player wins. if total score > random number, player loses. When player wins or loses, reset game
+
+    function resetGame() {
+
+    }
+
+    //If player wins, add 1 to the wins class. If player loses, add 1 to the losses class
     
     
 });
